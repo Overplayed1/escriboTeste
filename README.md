04c05743716d2b7a427b49d0caa3e740dce7a221
Overplayed1 <overplayed1@gmail.com>
2022-08-18 13:52:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Objetivos
+
+O objetivo deste programa é informar ao usuário o valor da soma de números divididos por 3 ou 5 que estão abaixo do valor informado pelo usuário.
+
+Exemplo: caso você informe 10, o valor exibido será 23, resultante da soma de 3, 5, 6 e 9.
+	Caso você informe 11, o valor exibido será 33, resultante da soma de 3, 5, 6, 9 e 10.
+
+## Como rodar/testar
+
+Para começar, você precisa ter o node instalado na sua máquina.\
+Abra o console, copie e cole este comando: node index.js.\
+Digite o número de seu interesse e cheque o resultado.
+
+#### `Feito utilizando JavaScript`# escriboTeste
---
+++ index.js
@@ -0,0 +1,46 @@
+// Chama a biblioteca "readline"
+var readline = require('readline');
+
+// Realiza a criação da interface desse processo
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Função responsável pela averiguação
+function somaTudo(value) {
+    // Transforma o valor em inteiro
+    const parsedValue = parseInt(value);
+
+    let soma = 0;
+
+    // Realiza o looping até o valor registrado anteriormente
+    for (let i = 0; i < parsedValue; i++) {
+
+        // Verifica se o valor é divisível por 3 ou por 5
+        if ((i % 3 === 0) || (i % 5 === 0)) {
+
+            // Realiza a soma caso seja divisível
+            soma = soma + i;
+        }
+    }
+
+    // Retorna o resultado de soma
+    return soma;
+
+}
+
+// O Readline importado é invocado para coletar o número.
+rl.question("Informe o número inteiro: ", (val) => {
+
+    //Constante com o resultado
+    const res = somaTudo(val)
+
+    // Log com o resultado
+    console.log("O resultado da soma dos números divisíveis por 3 e 5 que estão abaixo de " + val + " é " + res);
+
+    // Fecha o Readline para o programa não ficar executando infinitamente
+    rl.close();
+
+});
+
